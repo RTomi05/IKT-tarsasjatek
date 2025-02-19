4ab58065da4d81c71d269042c01f9f63e2b19f8f
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2025-02-19 10:35:07 +0100

    no

---
+++ roliverzio uj/penzes.js
@@ -0,0 +1,13 @@
+
+//játékosok pénze kezdésnél
+
+let /*Első játékos neve*/ = 0;
+let /*Második játékos neve*/ = 0;
+let /*Harmadik játékos neve*/ = 0;
+
+//játékosok pénze tranzakciónál
+
+let proba = 3000;
+let szerencseMozgas = -2000
+proba = proba + szerencseMozgas
+console.log(proba);
